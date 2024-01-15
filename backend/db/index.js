107ac4610294962e4b48c88f955142c6e351ec77
Zerro <sachitnair2002@gmail.com>
2024-01-15 14:39:19 +0530

    stuff happens

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose")
-const {MONGO_URL} = require("../.config.js")
+const { MONGO_URL } = require("../.config.js")
 
 mongoose.connect(MONGO_URL)
 
 const userSchema = new mongoose.Schema({
     username: String,
     password: String,
-    todos : [
+})
+
+const todoSchema = new mongoose.Schema({
+    username: String,
+    todos: [
         {
             title: String,
             description: String,
@@ -17,5 +21,6 @@ const userSchema = new mongoose.Schema({
 })
 
 const User = mongoose.model('User', userSchema)
+const Todo = mongoose.model("Todo", todoSchema)
 
-module.exports = {User}
+module.exports = { User, Todo}
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,41 @@
 const express = require("express");
-const PORT = 3000;
+const {userMiddleware} = require("./middleware/userMiddleware")
+const {User} = require("./db")
+const bodyParser = require("body-parser")
 
+const PORT = 3000;
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res)=>{
-    res.send("This Works!")
+    res.json({
+        msg: "welcome to the landing page"
+    })
+})
+
+app.post("/signup", async (req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    console.log(username, password)
+    try{    
+        await User.create({
+            username: username,
+            password: password
+        })
+    }
+    catch(e){
+        console.log(e)
+    }
+
+    res.json({
+        msg: "User created successfully!"
+    })
+})
+
+app.post("/signin", userMiddleware, (req, res)=>{
+    res.json("Control reaches here!")
 })
 
 app.listen(PORT)
