580b1268c5aeb410c091002a8bf2355ae725b410
Zerro <sachitnair2002@gmail.com>
2024-01-15 14:38:40 +0530

    Add sign up route

---
+++ backend/middleware/userMiddleware.js
@@ -0,0 +1,27 @@
+const {JWT_SECRET} = require("../.config.js")
+const jwt = require("jsonwebtoken")
+
+function userMiddleware(req, res, next){
+    const username = req.body.username;
+    const auth = req.headers.authorization
+    const token = auth.split(" ")[1]
+    try{
+        const decoded = jwt.verify(token, JWT_SECRET);
+        if(decoded.username){
+            next();
+        }
+        else{
+            res.status(403).json({
+                msg: "incorrect information sent, please try again."
+            })
+        }
+    }
+    catch(e){
+        res.status(403).json({
+            msg: "You are not authorized, please sign in first"
+        })
+    }
+
+}
+
+module.exports = {userMiddleware}
